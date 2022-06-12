c18c0428d5e0b4a646cd5a94bf25132b38181c2c
Himanshu <himanshu.sharma11199@gmail.com>
2022-06-12 12:36:58 +0530

    contains all project microservices

---
+++ DrugsMicroservice-master/DrugsMicroservice-master/src/test/java/com/cognizant/drugs/tests/controller/AppTest.java
@@ -0,0 +1,21 @@
+package com.cognizant.drugs.tests.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for simple App.
+ */
+@SpringBootTest
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void contextLoads(){
+
+    }
+
+}
---
+++ auth-service-main/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user (userid,upassword,uname) values ('a','test123','Himanshu');
+insert into user (userid,upassword,uname) values ('b','b','B');
+insert into user (userid,upassword,uname) values ('c','c','C');
+
