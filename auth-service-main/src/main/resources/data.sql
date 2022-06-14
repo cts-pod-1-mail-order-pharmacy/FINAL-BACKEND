71d1970127ce090afa37ea9e7ad9873b2cc73f8a
VedantWankhade <vedantwankhade17@gmail.com>
2022-06-14 16:49:06 +0530

    Remove the database initialization script

--- auth-service-main/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user (userid,upassword,uname) values ('a','test123','Himanshu');
-insert into user (userid,upassword,uname) values ('b','b','B');
-insert into user (userid,upassword,uname) values ('c','c','C');
-
